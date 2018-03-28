44bf398591280ca34bd70ce54e1c1912b60ee0e8
zanostorck <matheuszs@gmail.com>
2018-03-28 02:00:42 -0300

    Done

--- t1_wifi/t1_wifi.ino
+++ t1_wifi/t1_wifi.ino
@@ -1,53 +1,82 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-const byte maxLength = 16;
-char buffer[maxLength];
-int i=0;
+/*Configuração dos pinos do display LCD*/
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+/*Definições adicionais*/
+const byte maxLength = 32;  /*Tamanho máximo do buffer serial*/
+char buffer[maxLength];     /*Buffer serial*/
+boolean newData = false;    /*Controle do fluxo de dados via serial*/
 
 void setup() {
-  // Initialize both serial ports:
+  /*Inicializa serial: 19200 = BT baud rate*/
   Serial.begin(19200);
-  
-  // Initialize LCD display as 16x2:
+
+  /*Inicializa display e exibe mensagem de boas vindas*/
   lcd.begin(16,2);
+  lcd.print("Bem vindo!");
 }
 
 void loop() {
-  // read from port 1, send to port 0:
-  if (Serial.available()) {
-    for(i=0;i<maxLength;i++){
-      buffer[i] = Serial.read();
+  readData();
+  displayData();
+}
+
+/*---------------------------------------------------------
+  Função readData():
+  - Usada para ler os dados da serial
+  - Controle é realizado baseado no caractere de nova linha
+    '\n', pois quando este é recebido, é o sinal de que a 
+    transmissão da mensagem acabou
+  - Utiliza a variavel global newData para controle da 
+    exibição dos dados
+  ---------------------------------------------------------
+*/
+void readData(){
+  static byte i = 0;
+  char received;
+  char endMarker = '\n';
+  
+  while(Serial.available() > 0 && newData == false){
+    received = Serial.read();
+
+    if(received != endMarker){
+      buffer[i] = received;
+      i++;
+
+      if(i >= maxLength){
+        i = maxLength - 1;      
+      }      
     }
+    else{
+      buffer[i] = '\0';
+      i = 0;
+      newData = true;      
+    }  
+  } 
+}
+
+/*---------------------------------------------------------
+  Função displayData():
+  - Usada para exibir os dados no terminal e no display LCD
+  ---------------------------------------------------------
+*/
+void displayData(){
+  if(newData == true){
     
     Serial.print("Received: ");
-    Serial.print(buffer);
-    Serial.print("\n");
-  }
+    Serial.println(buffer);
+    
+    lcd.clear();
+    lcd.setCursor(0, 0); //First row
+    lcd.write(buffer);
 
-/*
- //Limpa a tela
-  lcd.clear();
-  //Posiciona o cursor na coluna 3, linha 0;
-  lcd.setCursor(3, 0);
-  //Envia o texto entre aspas para o LCD
-  lcd.print("FILIPEFLOP");
-  lcd.setCursor(3, 1);
-  lcd.print(" LCD 16x2");
-  delay(5000);
-  
-  //Rolagem para a esquerda
-  for (int posicao = 0; posicao < 3; posicao++)
-  {
-    lcd.scrollDisplayLeft();
-    delay(300);
-  }
-  
-  //Rolagem para a direita
-  for (int posicao = 0; posicao < 6; posicao++)
-  {
-    lcd.scrollDisplayRight();
-    delay(300);
-  }*/
-  
+    newData = false;
+  }  
 }
+
+
+
+  
+
